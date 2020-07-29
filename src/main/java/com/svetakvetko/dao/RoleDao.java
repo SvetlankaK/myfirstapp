389a96efe11745cf06d564369fa0379afd346813
Sveta Kvetko <svetlanko0604@mail.ru>
2020-07-29 10:19:15 +0300

    clipboardPatchFile

--- src/main/java/com/svetakvetko/dao/RoleDao.java
+++ src/main/java/com/svetakvetko/dao/RoleDao.java
@@ -1,4 +1,25 @@
 package com.svetakvetko.dao;
-//todo а оно мне надо?
+
+import com.svetakvetko.domain.Role;
+import com.svetakvetko.domain.User;
+
+
+import java.sql.ResultSet;
+import java.util.List;
+
+
 public interface RoleDao {
+
+    List<Long> getAllId(User user);
+
+    List<Role> getRolesById(Long userId);
+
+    List<Role> getRolesByLogin(String userLogin);
+
+    List<Role> getAllPossibleRoles();
+
+    Role extractRoleFromResultSet(ResultSet rs);
+
+    void deleteRoles(User user);
 }
+
---
+++ src/main/java/com/svetakvetko/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package com.svetakvetko.mapper;
+
+import com.svetakvetko.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.Collection;
+
+@Mapper
+@Repository
+public interface UserMapper {
+
+    Collection<User> findAll();
+
+    User findById(Long userId);
+
+    User findByLogin(String userLogin);
+
+    void delete(Long userId);
+
+    boolean isExist(String userLogin);
+
+    User update(User user);
+
+    void create(User user);
+}
